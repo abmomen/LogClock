da55840180727a3146bab1cee38cdb3e10ea3eea
Abdul Momen <abdulmomen130@gmail.com>
2026-08-14 22:08:35 +0600

    Initial Commit

---
+++ TinyTimer/TinyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTimerApp.swift
+//  TinyTimer
+//
+//  Created by Abdul Momen on 14/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TinyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
